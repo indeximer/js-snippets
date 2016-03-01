5590836176d74ae2957a02543c8a8380858f70b3
indeximer <douglasfortunato@hotmail.com>
2016-03-01 14:31:01 -0300

    Create my-tabs.js

---
+++ my-tabs.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+  $(".my-tabs-content").hide();
+    $(".my-tabs.active").each(function(){
+        var tab_ativa = $(this).attr("data-content");
+        $("#" + tab_ativa).show();
+    });
+});
+
+$(".my-tabs").click(function(){
+    var tab_group = $(this).data('rel');
+
+    $(".my-tabs[data-rel='"+ tab_group +"']").removeClass("active");
+    $(this).addClass("active");
+    $(".my-tabs-content[data-rel='"+ tab_group +"']").hide()
+    $("#"  + $(this).attr("data-content")).fadeIn();
+});
+
+$(".scroll-cotacao").click(function(){
+    $('html, body').animate({scrollTop: $('body').offset().top }, 'fast');
+});
