1170213fd14d642def4343cf56449022466dae61
indeximer <douglasfortunato@hotmail.com>
2016-04-14 01:54:04 -0300

    Update js-snippets.js

--- js-snippets.js
+++ js-snippets.js
@@ -1,4 +1,49 @@
 $(document).ready(function () {
+  
+  /***************************************
+   *************** MAP ******************/
+
+    var map;
+
+    var markerData = [
+        { lat: -19.526484 , lng: -40.635349, zoom: 16, name: "Colatina" },
+        { lat: -19.514385 , lng: -41.013371, zoom: 16, name: "Baixo Gandu" },
+        { lat: -18.772232 , lng: -40.885139, zoom: 16, name: "Barra de São Francisco" },
+    ];
+
+    function initialize () {
+        map = new google.maps.Map( document.getElementById( 'map-canvas' ), {
+            zoom: 16,
+            center: { lat: -19.526484 , lng: -40.635349 }
+        } );
+        markerData.forEach( function ( data ) {
+            var newmarker = new google.maps.Marker( {
+                map: map,
+                position: { lat: data.lat, lng: data.lng },
+                title: data.name
+            } );
+            jQuery( "#selectlocation" ).append( '<option value="' + [ data.lat, data.lng, data.zoom ].join( '|' ) + '">' + data.name + '</option>' );
+        } );
+
+    }
+
+    google.maps.event.addDomListener( window, 'load', initialize );
+
+    jQuery( document ).on( 'click', 'ul.list-map-locations li', function () {
+        $('ul.list-map-locations li').removeClass('active');
+        $(this).addClass('active');
+        var latlngzoom = jQuery( this ).data("coordinates").split( '|' );
+        var newzoom = 1 * latlngzoom[ 2 ],
+            newlat = 1 * latlngzoom[ 0 ],
+            newlng = 1 * latlngzoom[ 1 ];
+        map.setZoom( newzoom );
+        map.setCenter( { lat: newlat, lng: newlng } );
+    } );
+
+    /************** MAP ******************
+     **************************************/
+  
+  
   //initialize my-tabs
   $(".my-tabs-content").hide();
     $(".my-tabs.active").each(function(){
