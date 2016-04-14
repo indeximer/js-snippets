2de0af9788a1ed47eb1f5d5a016543fe54aa70fb
indeximer <douglasfortunato@hotmail.com>
2016-04-14 01:52:56 -0300

    Update and rename my-tabs.js to js-snippets.js

--- js-snippets.js
+++ js-snippets.js
@@ -1,11 +1,27 @@
 $(document).ready(function () {
+  //initialize my-tabs
   $(".my-tabs-content").hide();
     $(".my-tabs.active").each(function(){
         var tab_ativa = $(this).attr("data-content");
         $("#" + tab_ativa).show();
     });
-});
+    //initialize my-tabs
+    
+    //loadMore
+    var size_li = $(".load-more li").size();
+    var load = $(".load-more").data("load");
+    var inicial = $(".load-more").data("inicial");
+    var atual = inicial;
 
+    $('.load-more li:lt('+inicial+')').show();
+    $('#btn-load-more').click(function () {
+        atual= (atual + load <= size_li) ? atual + load : size_li;
+        $('.load-more li:lt('+atual+')').slideDown();
+    });
+    //loadMore
+});//document.ready
+
+// my-tabs
 $(".my-tabs").click(function(){
     var tab_group = $(this).data('rel');
     if($(this).siblings().length > 0){
@@ -15,3 +31,5 @@ $(".my-tabs").click(function(){
       $("#"  + $(this).attr("data-content")).fadeIn();
     }
 });
+// my-tabs
+
